d889fffb90ced2a7fd74fa2406d05bf9a0c03843
Pedro Krapinski Neto <pedrokn@gmail.com>
2022-01-08 08:32:47 -0300

    commit

---
+++ src/data/items.ts
@@ -0,0 +1,30 @@
+import { Item } from '../types/Item';
+export const items: Item[] = [
+    {
+        date: new Date(2022, 0, 8),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12,
+    },
+
+    {
+        date: new Date(2022, 0, 3),
+        category: 'food',
+        title: 'Burger King',
+        value: 28,
+    },
+
+    {
+        date: new Date(2022, 0, 1),
+        category: 'rent',
+        title: 'Aluguel casa',
+        value: 2300,
+    },
+
+    {
+        date: new Date(2022, 0, 2),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12,
+    }
+];
